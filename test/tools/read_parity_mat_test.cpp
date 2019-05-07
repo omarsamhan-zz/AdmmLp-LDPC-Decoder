e3692636e37002bc093c9d861762c9594069477e
Omar Samhan <omarsamhan90@gmail.com>
2019-05-07 09:14:20 -0400

    Made test work. Need to clean up the code (cmake) a bit.

--- test/tools/read_parity_mat_test.cpp
+++ test/tools/read_parity_mat_test.cpp
@@ -1,3 +1,10 @@
 #include "gtest/gtest.h"
 
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
+TEST(example, sum_zero) {
+  ASSERT_EQ(0, 0);
+}
